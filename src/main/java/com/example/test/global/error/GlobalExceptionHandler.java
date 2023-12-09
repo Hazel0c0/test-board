5d8fc971b5d2f2ff24d11e2baa17d274fad4192f
Hazel0c0 <secretwish5@naver.com>
2023-12-09 14:54:10 +0900

    Docs. board delete 주석 수정

--- src/main/java/com/example/test/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/test/global/error/GlobalExceptionHandler.java
@@ -16,7 +16,13 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleNoSuchElementException(NoSuchElementException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body("게시글을 찾을 수 없습니다.");
     }
-
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body("유효성 검사 실패: "
+            + ex.getBindingResult()
+            .getFieldError()
+            .getDefaultMessage());
+    }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleOtherExceptions(Exception e) {
         log.warn("기타 예외가 발생했습니다. - {}", e.getMessage());
