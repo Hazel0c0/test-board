d9ceb74582d5a07e588fac1a706e42c1545bed40
Hazel0c0 <secretwish5@naver.com>
2023-12-10 21:53:11 +0900

    완료!

--- src/main/java/com/example/test/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/example/test/global/error/GlobalExceptionHandler.java
@@ -19,6 +19,14 @@ public class GlobalExceptionHandler {
             .body("해당 번호"+msg+"을(를) 찾을 수 없습니다.");
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ResponseEntity.badRequest().body("유효성 검사 실패: "
+                + ex.getBindingResult()
+                .getFieldError()
+                .getDefaultMessage());
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleOtherExceptions(Exception e) {
         log.warn("기타 예외가 발생했습니다. - {}", e.getMessage());
